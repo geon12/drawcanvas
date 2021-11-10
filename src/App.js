cba5fee6aaef93955ded145eedeaf3734967b2de
Cameron <camcreel@gmail.com>
2021-11-09 19:11:02 -0500

    add canvas element

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// import {useState,useRef} from "react"
 
 function App() {
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <canvas
+        id="canvas"
+        
+        width={500}
+        height={500}
+        style={{
+          border: '2px solid #000',
+          marginTop: 10,
+        }}
+      ></canvas>
     </div>
   );
 }
