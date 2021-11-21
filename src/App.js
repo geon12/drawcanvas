a618fce4fdebb2f7e47977ee74e8f47b834ed96b
Cameron <camcreel@gmail.com>
2021-11-20 20:29:35 -0500

    Can draw with canvas component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-// import {useState,useRef} from "react"
+import Canvas from "./components/Canvas";
 
 function App() {
 
   
   return (
     <div>
-      <canvas
-        id="canvas"
-        
-        width={500}
-        height={500}
-        style={{
-          border: '2px solid #000',
-          marginTop: 10,
-        }}
-      ></canvas>
+      <Canvas />
     </div>
   );
 }
--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,13 +1,26 @@
-import { useRef } from "react"
+import { useEffect, useRef } from "react"
 
-function Canvas(props){
+export default function Canvas(props){
 
-    const canvasRef = useRef(null)
-    const canvas = canvasRef.current
-    const context = canvas.getContext('2d')
+    const canvasRef = useRef(null);
+
+    function draw(ctx) {
+        ctx.fillStyle = "#FF0000";
+        ctx.fillRect(0, 0, 150, 75);
+    };
+
+    useEffect(() => {
+    
+        const canvas = canvasRef.current
+        const context = canvas.getContext('2d')
+        
+        //Our draw come here
+        draw(context)
+    }, []);
+  
 
     return (
         <canvas ref={canvasRef} {...props} />
-    )
+    );
 
 }
